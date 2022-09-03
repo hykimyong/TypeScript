58165d019dffe25b13843fc8e06ecbf1cedc2604
hykimyong <hykimyong@naver.com>
2022-09-04 01:56:33 +0900

    readonly, any의 사용

--- src/Test.tsx
+++ src/Test.tsx
@@ -4,15 +4,24 @@ import React from 'react';
 type Age = number;
 type Name = string;
 type Player = {
-    name : Name,
+    readonly name : Name,
     age ?: Age
 }
+
 const playerMaker = (name:string) : Player => ({name});
 const nico = playerMaker('nico');
 nico.age = 12;
 
+//읽기전용 readonly tuple사용
+const players: readonly [string, number, boolean] = ['nico',1,true];
+//typescript의 보호장치를 비활성화시킴
+const a : any[] = [1,2,3,4];
+const b : any = true;
+
+
+
 function Test(){
-    return (<h3>nico</h3>);
+    return (<h3>nicos</h3>);
 }
 
 
