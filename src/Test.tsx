35f59e8a223756cb0d9cd7a028983ff2782d7cf2
hykimyong <hykimyong@naver.com>
2022-09-12 19:43:29 +0900

    오버로딩 매개변수

--- src/Test.tsx
+++ src/Test.tsx
@@ -7,27 +7,20 @@ type Player = {
     readonly name : Name,
     age ?: Age
 }
+//call signature
+//오버로딩 
+type Add = {
+    (a:number,b:number) :number
+    (a:number,b:number,c:number) :number
+}
+const add:Add =(a,b,c?:number)=> {
+    if(c) return a+b+c;
+    return a+b;
+}
 const playerMaker = (name:string) : Player => ({name});
 const nico = playerMaker('nico');
 nico.age = 12;
 
-let a: unknown;
-if(typeof a ==='number'){
-    let b = a+1;
-}
-if(typeof a === 'string'){
-    let b = a.toUpperCase();
-}
-
-function hello(name:string|number){
-    if(typeof name === 'string'){
-
-    }else if(typeof name === 'number'){
-        
-    }else{
-        
-    }
-} 
 
 
 function Test(){
